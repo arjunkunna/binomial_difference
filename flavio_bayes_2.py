cc90bd9bb4eb808ef9f3041f66c28cef45fd0d73
Arjun Kunna <arjunk1@stanford.edu>
2021-01-15 10:57:05 -0500

    commit

--- flavio_bayes_2.py
+++ flavio_bayes_2.py
@@ -6,30 +6,28 @@ import math
 
 
 #set true values of a and b
-a = 0.6
-b = 0.62
+a = 0.9
+b = 0.5
 q = a-b
 
-#set values of beta, n1, and n2
 beta_arr = [0.5, 0.75, 0.9, 0.95, 0.99]
 n1 = [1, 10, 100, 1000, 10000, 100000]
-n2 = [1, 10, 100, 1000, 10000, 100000]
+n2 = [100, 1000, 10000, 100000, 1000000, 10000000]
 sample_size = 500
 
+#generate data
 for k in range(len(n2)):
     for i in range(len(n1)):
-        #generate data
         data_x = binom.rvs(n1[i], a, size=sample_size)
         data_y = binom.rvs(n2[k], b, size=sample_size)
 
-        #compute beta parameters
         beta_a_1 = data_x + 1
         beta_a_2 = data_y + 1
 
         beta_b_1 = n1[i] - data_x + 1
         beta_b_2 = n2[k] - data_y + 1
 
-        #compute our credible interval
+
         for beta_iter in beta_arr:
             alpha = 1 - beta_iter
             lower_quantile = alpha/2
@@ -46,5 +44,5 @@ for k in range(len(n2)):
                     in_interval += 1
 
 
-            print("n1 = ", n1[i], "n2 = ", n2[k], "; beta = ", beta_iter, "; coverage = ", in_interval/sample_size)
-            print("Sample interval = ", credible_interval, "\n")
+        print("n1 = ", n1[i], "n2 = ", n2[k], "; beta = ", beta_iter, "; coverage = ", in_interval/sample_size)
+        print("Sample interval = ", credible_interval, "\n")
